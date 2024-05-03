b0df0bb1afe1db532e98875ba87047828a791c4e
Viktor Zhurbin <viktor.zhurbin@pumble.com>
2024-05-03 21:38:02 +0200

    set up million

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
 import react from "@vitejs/plugin-react-swc";
+import million from "million/compiler";
 import { defineConfig } from "vite";
 import checker from "vite-plugin-checker";
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [
+		million.vite({ auto: true }),
+		checker({ typescript: true }),
 		react(),
-		checker({
-			typescript: true,
-		}),
 	],
 });

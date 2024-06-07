69b442b95c69ecb5fde9b9cfbfdf308e6fe8beff
Viktor Zhurbin <viktor.zhurbin@pumble.com>
2024-06-07 22:41:09 +0200

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import react from "@vitejs/plugin-react-swc";
+import reactSwc from "@vitejs/plugin-react-swc";
 import million from "million/compiler";
 import { defineConfig } from "vite";
 import checker from "vite-plugin-checker";
@@ -6,8 +6,17 @@ import checker from "vite-plugin-checker";
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [
-		million.vite({ auto: true }),
-		checker({ typescript: true }),
-		react(),
+		million.vite({ auto: true, telemetry: false }),
+		checker({ typescript: true, enableBuild: true }),
+		reactSwc(),
 	],
+	build: {
+		rollupOptions: {
+			output: {
+				manualChunks: {
+					react: ["react", "react-dom"],
+				},
+			},
+		},
+	},
 });

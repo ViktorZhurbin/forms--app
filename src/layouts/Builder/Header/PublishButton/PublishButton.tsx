709796b81e978c8c3719ebb7fa96430e494a10b3
Viktor Zhurbin <viktor.zhurbin@pumble.com>
2024-06-28 16:06:00 +0200

    create dummy questions

--- src/layouts/Builder/Header/PublishButton/PublishButton.tsx
+++ src/layouts/Builder/Header/PublishButton/PublishButton.tsx
@@ -1,4 +1,16 @@
 import { Button, Tooltip } from "@mantine/core";
+import { QuestionGroups, QuestionTypes } from "~/constants/questions";
+import { createForm, createQuestion } from "~/models/methods";
+
+const fn = () =>
+	createQuestion({
+		formId: "b2485913-9276-4d87-bce6-0c0e0c997d41",
+		type: QuestionTypes.ShortText,
+		group: QuestionGroups.Text,
+		title: "ShortText",
+	});
+
+const fn2 = () => createForm({ name: "Form 2", responseCount: 10 });
 
 export const PublishButton = () => {
 	return (
@@ -7,7 +19,9 @@ export const PublishButton = () => {
 			arrowSize={6}
 			label="Make your changes visible to the world"
 		>
-			<Button color="rgb(31, 41, 55)">Publish</Button>
+			<Button onClick={fn} color="rgb(31, 41, 55)">
+				Publish
+			</Button>
 		</Tooltip>
 	);
 };
--- src/models/methods.ts
+++ src/models/methods.ts
@@ -1,4 +1,5 @@
 import { id, tx } from "@instantdb/react";
+import { QuestionTypes } from "~/constants/questions";
 import { db } from "./db";
 import type { TForm, TQuestion } from "./schema";
 
@@ -7,7 +8,38 @@ export function createForm(form: Omit<TForm, "id">) {
 }
 
 export function createQuestion(question: Omit<TQuestion, "id">) {
-	db.transact(tx.questions[id()].update(question));
+	const questionId = id();
+	const item = tx.questions[questionId];
+
+	switch (question.type) {
+		case QuestionTypes.YesNo:
+			db.transact(
+				item.update({
+					...question,
+					options: [
+						{ id: id(), text: "Yes" },
+						{ id: id(), text: "No" },
+					],
+				}),
+			);
+			break;
+
+		case QuestionTypes.MultipleChoice:
+			db.transact(
+				item.update({
+					...question,
+					options: [
+						{ id: id(), text: "Option 1" },
+						{ id: id(), text: "Option 2" },
+					],
+				}),
+			);
+			break;
+
+		case QuestionTypes.ShortText:
+			db.transact(item.update(question));
+			break;
+	}
 }
 
 export function updateQuestion(question: TQuestion) {

9559f69bffe2bbefb670a1dd95b4e72a2ac84af9
Viktor Zhurbin <viktor.zhurbin@pumble.com>
2024-07-22 00:21:33 +0200

    fix form name edit

--- apps/builder/src/layouts/Builder/Header/HeaderDemo.tsx
+++ apps/builder/src/layouts/Builder/Header/HeaderDemo.tsx
@@ -1,18 +1,28 @@
 import { EditableTextarea } from "@/shared/components/EditableTextarea/EditableTextarea";
+import { useFormNanoId } from "@/shared/hooks/useFormNanoId";
+import { useCurrentForm } from "@/shared/models/forms/read";
+import { updateForm } from "@/shared/models/forms/write";
 import { DarkModeToggle } from "~/components/DarkModeToggle/DarkModeToggle";
 import styles from "./Header.module.css";
-import { HomeButton } from "./HomeButton/HomeButton";
 import { PreviewButton } from "./PreviewButton/PreviewButton";
 import { PublishButton } from "./PublishButton/PublishButton";
 
 export const HeaderDemo = () => {
-	const onChangeTitle = () => {};
+	const formNanoId = useFormNanoId();
+	const form = useCurrentForm();
+
+	const onChangeTitle = (name: string) => {
+		updateForm({ nanoid: formNanoId, name });
+	};
 
 	return (
 		<div className={styles.root}>
 			<div className={styles.wrapper}>
-				<HomeButton />
-				<EditableTextarea tooltip="Rename" onChange={onChangeTitle} />
+				<EditableTextarea
+					tooltip="Rename"
+					initialValue={form?.name}
+					onChange={onChangeTitle}
+				/>
 			</div>
 			<div className={styles.wrapper}>
 				<PublishButton />

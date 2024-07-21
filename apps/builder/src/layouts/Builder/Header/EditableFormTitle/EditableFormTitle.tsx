42636ca89f1aa3a7ff9c945bb9e668f2e4c679c6
Viktor Zhurbin <viktor.zhurbin@pumble.com>
2024-07-22 01:16:07 +0200

    update styles

--- apps/builder/src/layouts/Builder/Header/EditableFormTitle/EditableFormTitle.tsx
+++ apps/builder/src/layouts/Builder/Header/EditableFormTitle/EditableFormTitle.tsx
@@ -1,21 +1,24 @@
-import { EditableTextarea } from "@/shared/components/EditableTextarea/EditableTextarea";
 import { useFormNanoId } from "@/shared/hooks/useFormNanoId";
 import { useCurrentForm } from "@/shared/models/forms/read";
 import { updateForm } from "@/shared/models/forms/write";
+import { TextInput, Tooltip } from "@mantine/core";
 
 export const EditableFormTitle = () => {
 	const form = useCurrentForm();
 	const formNanoId = useFormNanoId();
 
-	const onChangeTitle = (name: string) => {
+	const onChange = (name: string) => {
 		updateForm({ nanoid: formNanoId, name });
 	};
 
 	return (
-		<EditableTextarea
-			tooltip="Rename"
-			initialValue={form?.name}
-			onChange={onChangeTitle}
-		/>
+		<Tooltip withArrow label="Rename form">
+			<TextInput
+				value={form?.name ?? ""}
+				onChange={(event) => {
+					onChange?.(event.currentTarget.value);
+				}}
+			/>
+		</Tooltip>
 	);
 };

a67b04db1472e050f62a4205b090307906641543
Viktor Zhurbin <viktor.zhurbin@pumble.com>
2024-07-22 20:28:51 +0200

    update nanoid setup

--- libs/shared/src/utils/nanoid.ts
+++ libs/shared/src/utils/nanoid.ts
@@ -1,12 +1,12 @@
 import { customAlphabet } from "nanoId/non-secure";
 
-// -_ with https://github.com/CyberAP/nanoId-dictionary?tab=readme-ov-file#nolookalikessafe
-const noLookAlikesSafe = "6789BCDFGHJKLMNPQRTWbcdfghjkmnpqrtwz-_~";
+//  https://github.com/CyberAP/nanoId-dictionary?tab=readme-ov-file#nolookalikessafe
+const noLookAlikesSafe = "6789BCDFGHJKLMNPQRTWbcdfghjkmnpqrtwz";
 
 /**
  * Tested with https://zelark.github.io/nano-id-cc
  * With 1000 IDs generated per hour,
- * 14 days or 327K IDs needed,
+ * ~14 days or 327K IDs needed,
  * in order to have a 1% probability of at least one collision.
  */
 export const makeId = customAlphabet(noLookAlikesSafe, 8);

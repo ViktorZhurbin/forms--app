9f821ce2af5276d8cba198dceb3b54918f8103bd
Viktor Zhurbin <viktor.zhurbin@pumble.com>
2024-07-24 18:43:35 +0200

    fix preview modal visible height

--- apps/builder/src/layouts/Builder/modals/PreviewModal/PreviewModal.tsx
+++ apps/builder/src/layouts/Builder/modals/PreviewModal/PreviewModal.tsx
@@ -2,17 +2,21 @@ import { Form } from "@/shared/layouts/Form/Form";
 import { Button, Modal } from "@mantine/core";
 import { IconX } from "@tabler/icons-react";
 import type { ModalProps } from "../types";
+import styles from "./PreviewModal.module.css";
 
 export const PreviewModal = ({ isOpen, onClose }: ModalProps) => {
 	return (
 		<Modal
 			fullScreen
 			padding={0}
-			opened={isOpen}
-			withCloseButton={false}
 			withOverlay={false}
+			withCloseButton={false}
 			transitionProps={{ transition: "fade-down" }}
+			opened={isOpen}
 			onClose={onClose}
+			classNames={{
+				content: styles.modalContent,
+			}}
 		>
 			<Form
 				isPreview

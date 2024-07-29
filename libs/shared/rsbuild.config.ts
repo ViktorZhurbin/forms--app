b39fe6f29f736f5df5c112a95e331fb8ac4f1d39
Viktor Zhurbin <viktor.zhurbin@pumble.com>
2024-07-29 22:26:35 +0200

    shared config

---
+++ libs/shared/rsbuild.config.ts
@@ -0,0 +1,58 @@
+import { defineConfig } from "@rsbuild/core";
+import { pluginReact } from "@rsbuild/plugin-react";
+import { pluginSourceBuild } from "@rsbuild/plugin-source-build";
+import { pluginTypeCheck } from "@rsbuild/plugin-type-check";
+import { RsdoctorRspackPlugin } from "@rsdoctor/rspack-plugin";
+
+export default defineConfig(({ env, command, envMode }) => {
+	const isDev = env === "development";
+
+	// https://rsbuild.dev/config/output/css-modules#template-string
+	const localIdentName = isDev
+		? "[folder]__[local]-[hash:base64:6]"
+		: "[local]-[hash:base64:6]";
+
+	return {
+		html: {
+			template: "./static/index.html",
+		},
+		source: {
+			entry: {
+				index: "./src/main.tsx",
+			},
+		},
+		plugins: [pluginReact(), pluginTypeCheck(), pluginSourceBuild()],
+
+		output: {
+			cssModules: {
+				localIdentName,
+			},
+		},
+
+		performance: {
+			chunkSplit: {
+				strategy: "split-by-experience",
+				// forceSplitting: {
+				// 	"lib-ui": /node_modules[\\/](@mantine[\\/]*|@tabler\/*)/,
+				// 	"lib-instant": /node_modules[\\/]@instantdb\/*/,
+				// },
+			},
+		},
+
+		tools: {
+			rspack(config, { appendPlugins }) {
+				// Only register the plugin when RSDOCTOR is true, as the plugin will increase the build time.
+				if (process.env.RSDOCTOR) {
+					appendPlugins(
+						new RsdoctorRspackPlugin({
+							// plugin options
+							supports: {
+								generateTileGraph: true,
+							}
+						})
+					);
+				}
+			},
+		},
+	};
+});

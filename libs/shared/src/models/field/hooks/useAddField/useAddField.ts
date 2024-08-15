d5fa30d6697118214954aa5c7af3b6e5abd74b22
Viktor Zhurbin <viktor.zhurbin@pumble.com>
2024-08-15 08:56:54 +0200

    refactor useAddField

--- libs/shared/src/models/field/hooks/useAddField/useAddField.ts
+++ libs/shared/src/models/field/hooks/useAddField/useAddField.ts
@@ -1,12 +1,11 @@
-import { id, lookup, tx } from "@instantdb/react";
 import { useCallback } from "react";
 import type { FieldTypes } from "~/constants/field";
 import { useFormNanoId } from "~/hooks/useFormNanoId";
 import { useSelectedBlockId } from "~/hooks/useSelectedBlockId";
-import { db } from "~/models/db";
 import { getCreateFieldPayload } from "../../helpers/getCreateFieldPayload";
 import { getNewFieldOrder } from "../../helpers/getNewFieldOrder";
 import { useOrderedFormFields } from "../../read";
+import { createField, updateFieldsIndex } from "../../write";
 
 type AddFieldParams = {
 	type: FieldTypes;
@@ -31,18 +30,17 @@ export const useAddField = () => {
 
 			const newField = getCreateFieldPayload({ type, index: newFieldIndex });
 
-			// maintain array-like order of fields
-			const updateOrderOps = orderedFields.map(({ id }, index) => {
-				const newIndex = index >= newFieldIndex ? index + 1 : index;
-
-				return tx.fields[id].update({ index: newIndex });
+			await createField({
+				formNanoId,
+				payload: newField,
 			});
 
-			const createFieldOp = tx.fields[id()]
-				.update(newField)
-				.link({ forms: lookup("nanoId", formNanoId) });
+			// maintain array-like order of fields
+			const newFieldNanoIds = orderedFields
+				.toSpliced(newFieldIndex, 0, newField)
+				.map(({ nanoId }) => nanoId);
 
-			await db.transact([createFieldOp].concat(updateOrderOps));
+			await updateFieldsIndex(newFieldNanoIds);
 
 			return { nanoId: newField.nanoId };
 		},

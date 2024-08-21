53fcfc094c811f7808bfd5e0244b87943f479828
Viktor Zhurbin <viktor.zhurbin@pumble.com>
2024-08-21 11:19:09 +0200

    improve layout responsiveness

--- apps/editor/src/pages/form/tabs/Editor/Editor.tsx
+++ apps/editor/src/pages/form/tabs/Editor/Editor.tsx
@@ -1,8 +1,6 @@
 import { useCurrentFormQuery } from "@/shared/models/form/read";
-import { useMediaQuery } from "@mantine/hooks";
 import clsx from "clsx";
 import { NotFound } from "~/pages/NotFound/NotFound";
-import { SmallScreen } from "../../components/SmallScreen/SmallScreen";
 import styles from "./Editor.module.css";
 import { Main } from "./Main/Main";
 import { Navbar } from "./Navbar/Navbar";
@@ -10,16 +8,11 @@ import { SidebarSettings } from "./SidebarSettings/SidebarSettings";
 
 export const Editor = () => {
 	const { data } = useCurrentFormQuery();
-	const isTooSmall = useMediaQuery("(max-width: 668px)");
 
 	const formNotFound = data?.forms.length === 0;
 
 	if (formNotFound) {
 		return <NotFound title="We can't find this form" />;
-	}
-
-	if (isTooSmall) {
-		return <SmallScreen />;
 	}
 
 	const sidebarClass = clsx("sidebar", styles.sidebar);

7c6f8b50b91dba2cbb6af0d97e880604c2d165e7
Viktor Zhurbin <viktor.zhurbin@pumble.com>
2024-10-30 18:40:48 +0100

    web: test Next

---
+++ apps/web-next/app/layout.tsx
@@ -0,0 +1,26 @@
+import "@mantine/core/styles.css";
+import React from "react";
+import { MantineProvider, ColorSchemeScript } from "@mantine/core";
+import { theme } from "../theme";
+
+export const metadata = {
+  title: "Forms | Home"
+};
+
+export default function RootLayout({ children }: { children: any }) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <head>
+        <ColorSchemeScript />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
+        />
+      </head>
+      <body>
+        <MantineProvider theme={theme}>{children}</MantineProvider>
+      </body>
+    </html>
+  );
+}

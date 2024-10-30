13ca49bb7b85119baecac91f59774fdebc2b833c
Viktor Zhurbin <vzhurbin@gmail.com>
2024-10-31 00:37:07 +0100

    web: remove remix test

--- apps/web-remix/app/constants/links.ts
+++
@@ -1,8 +0,0 @@
-const APP_URL = "https://forms-editor.pages.dev";
-
-export const Links = {
-	SignIn: `${APP_URL}/signin`,
-	DemoBuilder: `${APP_URL}/create`,
-	Github: "https://github.com/ViktorZhurbin/forms--app",
-	Linkedin: "https://www.linkedin.com/in/viktor-zhurbin",
-};

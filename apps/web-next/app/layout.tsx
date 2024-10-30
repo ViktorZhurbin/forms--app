d3d48ba53ba64bebc668c079433cb714f49b93b1
Viktor Zhurbin <viktor.zhurbin@pumble.com>
2024-10-30 20:57:18 +0100

    update biome config

--- apps/web-next/app/layout.tsx
+++ apps/web-next/app/layout.tsx
@@ -1,26 +1,27 @@
 import "@mantine/core/styles.css";
-import React from "react";
-import { MantineProvider, ColorSchemeScript } from "@mantine/core";
+import { ColorSchemeScript, MantineProvider } from "@mantine/core";
 import { theme } from "../theme";
 
 export const metadata = {
-  title: "Forms | Home"
+	title: "Forms | Home",
 };
 
-export default function RootLayout({ children }: { children: any }) {
-  return (
-    <html lang="en" suppressHydrationWarning>
-      <head>
-        <ColorSchemeScript />
-        <link rel="shortcut icon" href="/favicon.ico" />
-        <meta
-          name="viewport"
-          content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
-        />
-      </head>
-      <body>
-        <MantineProvider theme={theme}>{children}</MantineProvider>
-      </body>
-    </html>
-  );
+export default function RootLayout({
+	children,
+}: { children: React.ReactNode }) {
+	return (
+		<html lang="en" suppressHydrationWarning>
+			<head>
+				<ColorSchemeScript />
+				<link rel="shortcut icon" href="/favicon.ico" />
+				<meta
+					name="viewport"
+					content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
+				/>
+			</head>
+			<body>
+				<MantineProvider theme={theme}>{children}</MantineProvider>
+			</body>
+		</html>
+	);
 }
